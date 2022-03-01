e6e37a8d3058eee3136d2ed3c7d1a31f973faae5
Globox1997 <mitschke11@web.de>
2022-03-01 21:27:52 +0100

    updated dependencies

--- src/main/java/net/fireplacemode/FireplaceMain.java
+++ src/main/java/net/fireplacemode/FireplaceMain.java
@@ -3,20 +3,19 @@ package net.fireplacemode;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.GameRules;
 
 public class FireplaceMain implements ModInitializer {
 
-    public static final Tag<Block> SAVE_BLOCK = TagFactory.BLOCK.create(new Identifier("fireplacemode", "save_block"));
+    public static final TagKey<Block> SAVE_BLOCK = TagKey.of(Registry.BLOCK_KEY, new Identifier("fireplacemode", "save_block"));
     public static final GameRules.Key<GameRules.BooleanRule> FIREPLACE_MODE = GameRuleRegistry.register("fireplaceMode", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
 
     @Override
     public void onInitialize() {
-
     }
 
 }
